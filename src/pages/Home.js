fbdc1072751a850d6e8fb956737b12818b402bb1
umairtouseef <umairtuseef41@gmail.com>
2025-01-22 16:44:31 +0500

    newpageadd

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,9 @@
-import EmojiGrid from "../components/EmogiContainer";
+import FifthBanner from "./FifthBanner";
 import FirstBanner from "./FirstBanner";
 import FourthBanner from "./FourthBanner";
 import SecoundBanner from "./SecoundBanner";
+import Footer from "./SeventhBanner";
+import SixthBanner from "./SixthBanner";
 import ThirdBanner from "./ThirdBanner";
 
 function Home() {
@@ -12,6 +14,9 @@ function Home() {
     <SecoundBanner/>
     <ThirdBanner/>
     <FourthBanner/>
+    <FifthBanner/>
+    <SixthBanner/>
+    <Footer/>
 
     </>
   );
